83c30b96b093649db00f4da77af068dcdb859899
jasminef <jasmine_fong@sfu.ca>
2019-02-09 14:50:07 -0800

    Change root coordinate to top left corner.

--- webpage/webview.js
+++ webpage/webview.js
@@ -15,21 +15,32 @@ function draw()
 	
 	ctx.fillStyle = '#404040';
 	
-	var size = 400;
+	
+	var size = 200;
 	var w = Math.sqrt(3)*size;
 	var h = 2*size;
 	var x = 400;
 	var y = 400;
 	ctx.beginPath();
 	
-	ctx.moveTo(x,y);
+	ctx.moveTo(x, y);
+	ctx.lineTo((x+(w/2)), (y-(h/4)));
+	ctx.lineTo((x+w), y);
+	ctx.lineTo((x+w), (y+(h/2)));
+	ctx.lineTo((x+(w/2)), (y+(h/2)+(h/4)));
+	ctx.lineTo(x, (y+(h/2)));
+	ctx.lineTo(x, y);
+	
+	ctx.stroke();
+/*
 	ctx.lineTo((x+(w/2)), (y+(h/4)));
 	ctx.lineTo((x+(w/2)), (y+(h/4)+(h/2)));
 	ctx.lineTo(x,(y+h));
 	ctx.lineTo((x-(w/2)), (y+(h/4)+(h/2)));
 	ctx.lineTo((x-(w/2)), (y+(h/4)));
 	ctx.lineTo(x,y);
-	
+
 	ctx.stroke();
-	
+*/
 }
+
