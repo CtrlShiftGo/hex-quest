55dcabb341a8e0845696a3f19055d2f3dfa643c3
jasminef <jasmine_fong@sfu.ca>
2019-02-09 14:27:46 -0800

    Draw single hexagon: calculation of points in progress, almost complete.

--- webpage/webview.js
+++ webpage/webview.js
@@ -1,18 +1,33 @@
 //test function
-function hello(){
+function hello()
+{
 	alert("hello world");
 }
 
-function draw() {
+function draw() 
+{
 	var canvas = document.getElementById("myCanvas");
-	if (canvas.getContext) {
-		var ctx = canvas.getContext('2d');
-		
-		ctx.beginPath();
-		ctx.moveTo(75, 50);
-		ctx.lineTo(100, 75);
-		ctx.lineTo(100, 25);
-		ctx.closePath();
-		ctx.stroke();
-	}
+	var ctx = canvas.getContext('2d');
+	
+	// set size of canvas to entire page
+	ctx.canvas.width = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+	
+	ctx.fillStyle = '#404040';
+	
+	var size = 400;
+	var w = Math.sqrt(3)*size;
+	var h = 2*size;
+	var x = 400;
+	var y = 400;
+	ctx.beginPath();
+	
+	ctx.moveTo(x,y);
+	ctx.lineTo((x+(w/2)), (y+(h/4)));
+	ctx.lineTo((x+(w/2)), (y+(h/4)+(h/2)));
+	ctx.lineTo(x,(y+h));
+	ctx.lineTo((x-(w/2)), (y+(h/4)+(h/2)));
+	
+	ctx.stroke();
+	
 }
